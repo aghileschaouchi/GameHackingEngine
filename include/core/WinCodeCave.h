01376f6222742b2cefd7fb7856b218e140eb3002
aghileschaouchi <aghiles.chaouchi@outlook.com>
2024-08-24 13:54:19 +0200

    wip2

--- include/core/WinCodeCave.h
+++ include/core/WinCodeCave.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <Psapi.h>
@@ -6,11 +8,6 @@
 
 namespace ghe
 {
-	/*class reverseEngine
-	{
-
-	};*/
-
 	namespace winCodeCave
 	{
 		namespace ASM
@@ -77,51 +74,51 @@ namespace ghe
 			return true;
 		}
 
-		bool WriteIntoMemory2(uintptr_t destAddress, LPVOID patch, size_t numBytes)
-		{
-			unsigned long  oldProtection = 0;
-			// Store the source address
-			DWORD srcAddress = PtrToUlong(patch);
+		//bool WriteIntoMemory2(uintptr_t destAddress, LPVOID patch, size_t numBytes)
+		//{
+		//	unsigned long  oldProtection = 0;
+		//	// Store the source address
+		//	DWORD srcAddress = PtrToUlong(patch);
 
-			if (VirtualProtect(reinterpret_cast<LPVOID>(destAddress), static_cast<size_t>(numBytes), PAGE_EXECUTE_READWRITE, &oldProtection) == NULL)
-			{
-				printf("WriteIntoMemory2() exited with %ud as code error, check VirtualProtect call 1\n", GetLastError());
-				return false;
-			}
+		//	if (VirtualProtect(reinterpret_cast<LPVOID>(destAddress), static_cast<size_t>(numBytes), PAGE_EXECUTE_READWRITE, &oldProtection) == NULL)
+		//	{
+		//		printf("WriteIntoMemory2() exited with %ud as code error, check VirtualProtect call 1\n", GetLastError());
+		//		return false;
+		//	}
 
-			//SHOULD FIX THIS PART! use "__asm" for each line?
-			//__asm
-			//{
-			//	nop
-			//	nop
-			//	nop
+		//	//SHOULD FIX THIS PART! use "__asm" for each line?
+		//	//__asm
+		//	//{
+		//	//	nop
+		//	//	nop
+		//	//	nop
 
-			//	mov esi, srcAddress		// Save the address
-			//	mov edi, destAddress	// Save the destination address
-			//	mov ecx, numBytes		// Save the size of the patch
-			//	Start :
-			//	cmp ecx, 0				// Are we done yet?
-			//		jz Exit					// If so, go to end of function
+		//	//	mov esi, srcAddress		// Save the address
+		//	//	mov edi, destAddress	// Save the destination address
+		//	//	mov ecx, numBytes		// Save the size of the patch
+		//	//	Start :
+		//	//	cmp ecx, 0				// Are we done yet?
+		//	//		jz Exit					// If so, go to end of function
 
-			//		mov al, [esi]			// Move the byte at the patch into AL
-			//		mov[edi], al			// Move AL into the destination byte
-			//		dec ecx					// 1 less byte to patch
-			//		inc esi					// Next source byte
-			//		inc edi					// Next destination byte
-			//		jmp Start				// Repeat the process
-			//		Exit :
-			//	nop
-			//		nop
-			//		nop
-			//}
+		//	//		mov al, [esi]			// Move the byte at the patch into AL
+		//	//		mov[edi], al			// Move AL into the destination byte
+		//	//		dec ecx					// 1 less byte to patch
+		//	//		inc esi					// Next source byte
+		//	//		inc edi					// Next destination byte
+		//	//		jmp Start				// Repeat the process
+		//	//		Exit :
+		//	//	nop
+		//	//		nop
+		//	//		nop
+		//	//}
 
-			if (VirtualProtect(reinterpret_cast<LPVOID>(destAddress), static_cast<size_t>(numBytes), oldProtection, NULL) == NULL)
-			{
-				printf("writeIntoMemory1() exited with %ud as code error, check VirtualProtect call 2\n", GetLastError());
-				return false;
-			}
-			return true;
-		}
+		//	if (VirtualProtect(reinterpret_cast<LPVOID>(destAddress), static_cast<size_t>(numBytes), oldProtection, NULL) == NULL)
+		//	{
+		//		printf("writeIntoMemory1() exited with %ud as code error, check VirtualProtect call 2\n", GetLastError());
+		//		return false;
+		//	}
+		//	return true;
+		//}
 
 		//by default its a jump code cave
 		template<unsigned int INSTRUCTION = ASM::JMP>
@@ -160,38 +157,38 @@ namespace ghe
 			return true;
 		}
 
-		template<unsigned int INSTRUCTION = ASM::CALL>
-		bool codeCave2(uintptr_t destAddress, VOID(*func)(VOID), BYTE nopCount)
-		{
-			//calculate the code cave for chat interception : offset = (PtrToUlong(func) - destAddress) - 5
-			DWORD offset = (reinterpret_cast<DWORD>(func) - static_cast<DWORD>(destAddress)) - 5;
+		//template<unsigned int INSTRUCTION = ASM::CALL>
+		//bool codeCave2(uintptr_t destAddress, VOID(*func)(VOID), BYTE nopCount)
+		//{
+		//	//calculate the code cave for chat interception : offset = (PtrToUlong(func) - destAddress) - 5
+		//	DWORD offset = (reinterpret_cast<DWORD>(func) - static_cast<DWORD>(destAddress)) - 5;
 
-			//buffer of NOPs, static since we limit to 'UCHAR_MAX' NOPs
-			BYTE nopPatch[0xFF] = { 0 };
+		//	//buffer of NOPs, static since we limit to 'UCHAR_MAX' NOPs
+		//	BYTE nopPatch[0xFF] = { 0 };
 
-			BYTE patch[5] = { 0x00, 0x00, 0x00, 0x00, 0x00 };
-			if constexpr (INSTRUCTION == ASM::CALL)
-			{
-				patch[0] = ASM::CALL;
-			}
+		//	BYTE patch[5] = { 0x00, 0x00, 0x00, 0x00, 0x00 };
+		//	if constexpr (INSTRUCTION == ASM::CALL)
+		//	{
+		//		patch[0] = ASM::CALL;
+		//	}
 
-			if constexpr (INSTRUCTION == ASM::JMP)
-			{
-				patch[0] = ASM::JMP;
-			}
+		//	if constexpr (INSTRUCTION == ASM::JMP)
+		//	{
+		//		patch[0] = ASM::JMP;
+		//	}
 
-			memcpy(patch + 1, &offset, sizeof(DWORD));
-			if (!WriteIntoMemory2(destAddress, static_cast<size_t>(patch), 5))
-			{
-				return false;
-			}
+		//	memcpy(patch + 1, &offset, sizeof(DWORD));
+		//	if (!WriteIntoMemory2(destAddress, static_cast<size_t>(patch), 5))
+		//	{
+		//		return false;
+		//	}
 
-			if (nopCount == 0)
-				return true;
+		//	if (nopCount == 0)
+		//		return true;
 
-			memset(nopPatch, ASM::NOP, nopCount);
+		//	memset(nopPatch, ASM::NOP, nopCount);
 
-			return WriteIntoMemory2(destAddress + 5, reinterpret_cast<LPVOID>(nopPatch), static_cast<size_t>(nopCount));
-		}
+		//	return WriteIntoMemory2(destAddress + 5, reinterpret_cast<LPVOID>(nopPatch), static_cast<size_t>(nopCount));
+		//}
 	}
 }
